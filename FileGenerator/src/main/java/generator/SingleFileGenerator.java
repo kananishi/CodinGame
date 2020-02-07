32057d2eb9a85033276d2d1f0057cf9b6b8a6875
Guilherme Nishi Kanashiro <kanashiro.nishi@gmail.com>
2020-02-07 10:53:56 -0300

    Refatoração do gerador de arquivo único

--- FileGenerator/src/main/java/generator/SingleFileGenerator.java
+++ FileGenerator/src/main/java/generator/SingleFileGenerator.java
@@ -1,21 +1,28 @@
-package convert.classes.into.singleFile;
+package generator;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.channels.FileChannel;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.event.ListSelectionEvent;
 
-public class GenearateSingleFileFromClasses {
 
-	private static final String sourceFolder = "C:\\Users\\Guilherme\\Documents\\Work\\sources\\CodingGames\\";
+public final class SingleFileGenerator {
+
+	private static final String sourceFolder = System.getProperty("user.home") + "\\Documents\\Work\\sources\\CodingGames\\";
 	private static final String javaProject = "src\\main\\java\\";
 	
-	private static final String actualGame = "CodeVsZombies\\";
+	private static final String actualGame = "CodeOfIceAndFire\\";
 	private static final String gameFolder = "game\\";
 	
 	private static final String fileGenerator = "FileGenerator\\";
@@ -26,7 +33,7 @@ public class GenearateSingleFileFromClasses {
 			+ "import java.math.*;\n"
 			+ "import static java.util.stream.Collectors.toList;";
 
-	public static void main(final String[] args) {
+	public static final void main(final String[] args) {
 		try {
 			run();
 			System.out.println("Sucess!!");
@@ -45,33 +52,49 @@ public class GenearateSingleFileFromClasses {
 		final PrintWriter pw = new PrintWriter(new FileWriter(fout));
 		pw.print(defaultImport);
 
-		final Path dir = Paths.get(sourceFolder, actualGame, javaProject, gameFolder);
-		final DirectoryStream<Path> files = Files.newDirectoryStream(dir);
-		for(final Path path : files) {
-			final Scanner in = new Scanner(path.toFile());
-			while(in.hasNextLine()) {
-				final String line = in.nextLine();
-				if(ignoreOnCompile(line)) {
-					pw.println(line);
-				}
-			}
-		}
+		final File dir = new File(sourceFolder + actualGame + javaProject + gameFolder);
+		concatenateDirectoryFiles(dir, pw);
 		pw.flush();
 		pw.close();
+	}
+
+	private static void concatenateDirectoryFiles(final File dir, final PrintWriter fout) {
+		if(dir.isDirectory()) {
+			for(File file : dir.listFiles()) {
+				concatenateDirectoryFiles(file, fout);
+			}
+		}
+		if(dir.isFile()) {
+			copyFileToNewFile(dir, fout);
+		}
+		return;
+	}
+
+	private static void copyFileToNewFile(final File file, final PrintWriter fout) {
+		try {
+			final Scanner in = new Scanner(file);
+			while(in.hasNextLine()) {
+				final String line = in.nextLine();
+				if(!ignoreOnCompile(line)) {
+					fout.println(line);
+				}
+			}
+			in.close();
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 	/**
 	 * Retorna false se a linha for package, import, ou comentario
 	 */
 	private static boolean ignoreOnCompile(final String line) {
-		return !line.startsWith("package")
-				&& !line.startsWith("import")
-				&& !line.startsWith("//")
-				&& !line.startsWith("/**")
-				&& !line.startsWith("*")
-				&& !line.startsWith("*/");
+		return line.startsWith("package")
+				|| line.startsWith("import")
+				|| line.startsWith("//")
+				|| line.startsWith("/**")
+				|| line.startsWith("*")
+				|| line.startsWith("*/");
 	}
-
-
 
 }
